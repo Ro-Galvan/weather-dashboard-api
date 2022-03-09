f30a300bc71402c74f038a9c0bcd4936f4c3925f
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-08 19:06:22 -0500

    edited 2nd API and created divs for the 5 day forecast

--- script.js
+++ script.js
@@ -41,11 +41,12 @@ var getCityWeather = function (city) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);  
+        console.log(data);
+        requestForecastData(data.coord.lat);  
         
         // city name + date --CONVERT  +  weather icon 
         var tempInfo = document.createElement('div');
-        tempInfo.textContent = data.name + "  (" + data.dt + ") " + data.weather[0].icon
+        tempInfo.innerHTML = data.name + "  (" + data.dt + ") " + data.weather[0].icon
         currentCityWeatherEl.appendChild(tempInfo);
 
         // temp
@@ -62,38 +63,58 @@ var getCityWeather = function (city) {
         var tempInfo = document.createElement('div');
         tempInfo.textContent = "Humidity: " + data.main.humidity + " %"
         currentCityWeatherEl.appendChild(tempInfo);
-      
-        // 2nd API call 
-        var requestForecastURL= 'https://api.openweathermap.org/data/2.5/onecall?lat=35.779591&lon=-78.638176&appid=c7c6d80dddc4d33a547a4a23dbe7b20c&units=imperial';
 
-        fetch(requestForecastURL)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-                
-                var forecastInfo = document.createElement('div');
-                forecastInfo.textContent = data.current.uvi + " %"
-                currentCityWeatherEl.appendChild(forecastInfo);
+
         
-                // 5 day forecast
-                var forecastInfo = document.createElement('div');
-                forecastInfo.textContent = data.current.uvi
-                forecastEl.appendChild(forecastInfo);
-                
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-        // UV index 
     })
 };
-// One Call API for UVI and forecast
-var loadForecast = function (display) {
-// if city name data displays then display 5 day forecast
+// var lat =
+// var lon = 
+//  might not be needed
+var requestForecastData = function (lat, lon) {
+            // 2nd API call 
+            var requestForecastURL= 'https://api.openweathermap.org/data/2.5/onecall?lat=35.779591&lon=-78.638176&appid=c7c6d80dddc4d33a547a4a23dbe7b20c&units=imperial';
+
+            fetch(requestForecastURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    // UV index 
+                    var forecastInfo = document.createElement('div');
+                    forecastInfo.textContent = data.current.uvi + " %"
+                    currentCityWeatherEl.appendChild(forecastInfo);
+            
+                    for (let i = 0; i <= 4; i++) {
+                        // 5 day forecast
+                        // to avoid repeat
+                        var currentDay = data.daily[i];
+                
+                        var forecastInfo = document.createElement("div");
+                        forecastInfo.innerHTML =`
+                            <p>${currentDay.dt}</p>
+                            <img src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}.png" />
+                            <p>${currentDay.temp.day}</p>
+                            <p>${currentDay.wind_speed}</p>
+                            <p>${currentDay.humidity}</p>
+                            `;
+                        console.log(currentDay.weather[0].icon);
+                        forecastInfo.style.border = "1px solid black";
+                        forecastEl.appendChild(forecastInfo);
+                      }
+                    
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+                
 }
+
+// // One Call API for UVI and forecast
+// var loadForecast = function (display) {
+// // if city name data displays then display 5 day forecast
+// }
 
 
 // var getCityWeather = function (city) { 
@@ -102,3 +123,19 @@ var loadForecast = function (display) {
 //   listens for when FORM is clicked
 userFormEl.addEventListener('submit', formSubmitHandler);
     console.log('form clicked worked');
+
+    
+
+    // EXTRA
+    // var getFeaturedRepos = function (language) {
+    //     var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+      
+    //     fetch(apiUrl).then(function (response) {
+    //       if (response.ok) {
+    //         response.json().then(function (data) {
+    //           displayRepos(data.items, language);
+    //         });
+    //       } else {
+    //         alert('Error: ' + response.statusText);
+    //       }
+    //     });
